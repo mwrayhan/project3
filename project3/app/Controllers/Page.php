30bf780345c799c741bf9037f2b37aecf2cc7b82
Raihanul mawa <anjingtanah123.rm@gmail.com>
2025-07-18 10:45:01 +0700

    Modify routes and add controller-view for home, about, etc

--- project3/app/Controllers/Page.php
+++ project3/app/Controllers/Page.php
@@ -18,4 +18,5 @@ class Page extends BaseController
     {
         echo view("Faqs");
     }
+    
 }
--- project3/app/Views/faqs.php
+++ project3/app/Views/faqs.php
@@ -1,27 +1,82 @@
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pertanyaan</title>
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>MyBlog</title>
+
+	<!-- Bootstrap CSS -->
+	<link rel="stylesheet" href="<?= base_url('css/bootstrap.min.css') ?>" />
 </head>
+
 <body>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12 my-2 card">
-                <div class="card-body">
-                    <h5 class="h5">Pertanyaan? (Q)</h5>
-                    <p>(A) Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-                </div>
-            </div>
-        </div>
+
+	<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+		<div class="container">
+			<a class="navbar-brand" href="<?= base_url() ?>">MyBlog</a>
+			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarNav">
+				<ul class="navbar-nav">
+					<li class="nav-item">
+						<a class="nav-link" href="<?= base_url() ?>">Home</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="<?= base_url('about') ?>">About</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="<?= base_url('post') ?>">Blog</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="<?= base_url('contact') ?>">Contact</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link active" aria-current="page" href="<?= base_url('faqs') ?>">FAQ</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+
+	<div class="p-5 mb-4 bg-light rounded-3">
+      <div class="container py-5">
+        <h1 class="display-5 fw-bold">FAQ</h1>
+       
+      </div>
     </div>
 
-    <!-- Bootstrap JS and dependencies -->
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12 my-2 card">
+				<div class="card-body">
+					<h5 class="h5">Pertanyaan? (Q)</h5>
+					<p>(A) Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam perferendis commodi tenetur quos ducimus repellat nulla, nam magni. Commodi iusto ad harum voluptas exercitationem facere eos earum laboriosam excepturi quas?</p>
+				</div>
+			</div>
+			<div class="col-md-12 my-2 card">
+				<div class="card-body">
+					<h5 class="h5">Pertanyaan? (Q)</h5>
+					<p>(A) Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam perferendis commodi tenetur quos ducimus repellat nulla, nam magni. Commodi iusto ad harum voluptas exercitationem facere eos earum laboriosam excepturi quas?</p>
+				</div>
+			</div>
+			
+		</div>
+	</div>
+	<div class="container py-4">
+		<footer class="pt-3 mt-4 text-muted border-top">
+			<div class="container">
+				&copy; <?= Date('Y') ?>
+			</div>
+		</footer>
+	</div>
+
+	<!-- Jquery dan Bootsrap JS -->
+	<script src="<?= base_url('js/jquery.min.js') ?>"></script>
+	<script src="<?= base_url('js/bootstrap.min.js') ?>"></script>
+
 </body>
+
 </html>
